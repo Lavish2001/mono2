523ac66ea7f90a5e212a1eaf636ff1ae5c215bcc
Lavish <lavish.delta4infotech@gmail.com>
2024-09-27 16:25:04 +0530

    Update package.json and database model paths

--- packages/database/models/MongoDB/index.js
+++ packages/database/models/MongoDB/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Mongoose = require('mongoose');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../../config/database.js')[env]["mongodb"];
+const config = require(__dirname + '/../../config/database.js')[env]["mongodb"];
 
 if (config.url) {
   Mongoose.connect(config.url, config.options);
--- packages/database/models/index.js
+++ packages/database/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/database.js')[env]["mysql"];
+const config = require(__dirname + '/../config/database.js')[env]["mysql"];
 const db = {};
 
 let sequelize;
