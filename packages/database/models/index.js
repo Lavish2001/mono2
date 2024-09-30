c699fd7436df0f0ee2482bbb57f9d686b9135cca
Lavish <lavish.delta4infotech@gmail.com>
2024-09-30 17:16:17 +0530

    Refactor database models and connections

--- packages/database/models/index.js
+++ packages/database/models/index.js
@@ -2,9 +2,18 @@
 
 const fs = require('fs');
 const path = require('path');
+const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const models = {};
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/database.js')[env]["mysql"];
+const db = {};
 
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
@@ -12,8 +21,17 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file));
-    models[file] = model;
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
   });
 
-module.exports = models;
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
